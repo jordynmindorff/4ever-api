42b3260a1cc863c50a1acebbc3496856a13ccd1e
Jordyn <jordyn.mindorff@gmail.com>
2023-07-18 22:56:04 -0400

    Setup memories route to actually return memories

--- controllers/memory.js
+++ controllers/memory.js
@@ -2,14 +2,17 @@ import { client } from '../utils/db.js';
 import status from 'http-status';
 import ErrorResponse from '../utils/errorResponse.js';
 
+// @desc get all memories associated with a user account
+// @route GET /v1/memory/
+// @access PRIVATE
 export const getMemories = async (req, res, next) => {
 	try {
 		const { user: id } = req;
-		const users = await client.query('SELECT * FROM public.user WHERE id=$1', [id]);
+		const memories = await client.query('SELECT * FROM public.memory WHERE user_id=$1', [id]);
 
 		return res.json({
 			success: true,
-			data: users.rows,
+			data: memories.rows,
 		});
 	} catch (err) {
 		next(new ErrorResponse('Server Error', status.INTERNAL_SERVER_ERROR, err));
