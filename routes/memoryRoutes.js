c7e140872f6598fb06f1cb5d92403c095c155bc1
Jordyn <jordyn.mindorff@gmail.com>
2023-07-20 22:53:08 -0400

    Let's call it CRD functionality, just need the the U

--- routes/memoryRoutes.js
+++ routes/memoryRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import protect from '../middleware/auth.js';
 
-import { getMemories } from '../controllers/memory.js';
+import { getMemories, createMemory, deleteMemory } from '../controllers/memory.js';
 
 const memoriesRouter = express.Router();
 
-memoriesRouter.route('/').get(protect, getMemories);
+memoriesRouter
+	.route('/')
+	.get(protect, getMemories)
+	.post(protect, createMemory)
+	.delete(protect, deleteMemory);
 
 export default memoriesRouter;
