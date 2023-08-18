1c152be96d4d1d1d6ab6634df85e66bc86694f4c
Jordyn <jordyn.mindorff@gmail.com>
2023-08-18 14:58:16 -0400

    Improve validation errors

--- validation/UserValidationSchema.js
+++ validation/UserValidationSchema.js
@@ -2,16 +2,24 @@ import { object, string, date } from 'yup';
 
 export default object({
 	username: string()
-		.required()
+		.required('A Username is Required')
 		.lowercase()
 		.min(1)
 		.max(50)
 		.matches(/^[a-z0-9_-]+$/i),
 	password: string()
-		.required()
+		.required('A Password is Required')
 		.min(8)
 		.max(72)
-		.matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_-])[A-Za-z\d@$!%*?&_]{8,72}$/),
-	email: string().required().email().lowercase(),
-	birthday: date().required(),
+		.matches(
+			/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[@$!%*?&_-]).{8,72}$/,
+			'Password must be between 8-72 characters, including a number, an uppercase letter, a lowercase letter, and a special character.'
+		)
+		.typeError('Invalid Password'),
+	email: string()
+		.required('An Email is Required')
+		.email('Invalid Email Address')
+		.lowercase()
+		.typeError('Invalid Email'),
+	birthday: date('Invalid Date').required('A Birthday is Required').typeError('Invalid Date'),
 });
